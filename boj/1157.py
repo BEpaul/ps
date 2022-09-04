651d3fae40fcfae2e512bbabd676eccccd4bcd31
Minhyuk Kim <rhrlwld@gmail.com>
2022-09-04 20:46:28 +0900

    6단계

--- boj/1157.py
+++ boj/1157.py
@@ -8,6 +8,5 @@ h_count_list = []
 for l in l_alpha:
     l_count_list.append(input_str.count(l))
 
-print(l_count_list.index('h'))
-    
-        
+print(l_count_list.index(max(l_count_list)))
+l_list_index = l_count_list.index(max(l_count_list))
