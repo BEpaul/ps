d0ef41a2d2a87f072cecfd84cdffc4157a1b8abc
Minhyuk Kim <rhrlwld@gmail.com>
2022-09-05 00:16:19 +0900

    6단계

--- boj/1157.py
+++ boj/1157.py
@@ -1,12 +1,13 @@
-l_alpha = 'abcdefghijklmnopqrstuvwxyz'
-h_alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+input_str = input().upper()
+count_list = []
 
-input_str = input()
-l_count_list = []
-h_count_list = []
+for alpha in alphabet:
+    count_list.append(input_str.count(alpha))
+    
+sorted_list = sorted(count_list)
 
-for l in l_alpha:
-    l_count_list.append(input_str.count(l))
-
-print(l_count_list.index(max(l_count_list)))
-l_list_index = l_count_list.index(max(l_count_list))
+if sorted_list[len(sorted_list) - 1] == sorted_list[len(sorted_list) - 2]:
+    print('?')
+else:
+    print(alphabet[count_list.index(max(count_list))])
