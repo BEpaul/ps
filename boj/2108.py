0132d714d513df549720cf78260643cadc3e5380
Minhyuk Kim <rhrlwld@gmail.com>
2022-10-03 23:12:18 +0900

    9단계

---
+++ boj/2108.py
@@ -0,0 +1,30 @@
+from audioop import avg
+from collections import Counter
+
+N = int(input())
+num_list = []
+for i in range(N):
+    num_list.append(int(input()))
+
+num_list.sort()
+
+# 산술평균
+print(round(sum(num_list) / len(num_list)))
+
+# 중앙값
+print(num_list[int((len(num_list)+ 1)/2) - 1])
+
+# 최빈값
+cnt = Counter(num_list).most_common(2)
+
+if len(num_list) > 1:
+    if cnt[0][1] == cnt[1][1]:
+        print(cnt[1][0])
+    else:
+        print(cnt[0][0])
+else:
+    print(cnt[0][0])
+    
+# 차
+print(max(num_list) - min(num_list))
+
