5a99997ccf3d31f53fb619c9f30409aa7cc8b3d9
Minhyuk Kim <rhrlwld@gmail.com>
2022-10-15 22:51:01 +0900

    13단계

---
+++ boj/4153.py
@@ -0,0 +1,11 @@
+while(True):
+    a, b, c = map(int, input().split())
+    
+    if a == 0 and b == 0 and c == 0:
+        break
+
+    else:
+        if pow(a, 2) + pow(b, 2) == pow(c, 2):
+            print("right")
+        else:
+            print("wrong")
