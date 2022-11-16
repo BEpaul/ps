2a58c2ac4fecf6dadae9704d696b1ddc5e1bd409
Minhyuk Kim <rhrlwld@gmail.com>
2022-11-16 20:39:57 +0900

    그리디

---
+++ boj/11047.py
@@ -0,0 +1,21 @@
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+
+value_list = []
+
+for _ in range(N):
+    value = int(sys.stdin.readline())
+    value_list.append(value)
+
+value_list.reverse()
+
+count = 0
+for idx in range(N):
+    quo = K // value_list[idx]
+    count += quo
+    rem = K % value_list[idx]
+    K = rem
+
+print(count)
+
