b6ce0dcd68fcde34421f6dd4804e1129876ef3c7
Minhyuk Kim <rhrlwld@gmail.com>
2023-01-13 00:02:02 +0900

    9단계

--- boj/단계별/2563.py
+++ boj/단계별/2563.py
@@ -1,4 +1,15 @@
+arr = [[0 for _ in range(101)] for _ in range(101)]
 N = int(input())
 
 for _ in range(N):
-    A, B = map(int, input().split())
+    x, y = map(int, input().split())
+
+    for row in range(x, x+10):
+        for col in range(y, y+10):
+            arr[row][col] = 1
+
+res = 0
+for i in range(101):
+    res += sum(arr[i])
+
+print(res)
