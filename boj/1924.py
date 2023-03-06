149f8b30e74ce13edf01fc821a079f10845d9c47
Minhyuk Kim <kim.minhyuk@mintech.kr>
2023-03-06 23:53:47 +0900

    구현

---
+++ boj/1924.py
@@ -0,0 +1,12 @@
+days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+last = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+X, Y = map(int, input().split())
+day = 0
+
+for i in range(0, X - 1):
+    day += last[i]
+
+day = (day + Y) % 7
+
+print(days[day])
