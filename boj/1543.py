664d4c0430bedaba3e2619a3cdeee3f0425dd00d
Minhyuk Kim <rhrlwld@gmail.com>
2023-07-04 23:37:00 +0900

    문자열

--- boj/1543.py
+++ boj/1543.py
@@ -11,3 +11,10 @@ while True:
         count += 1
 
 print(count)
+
+# 짧은 풀이... split()을 이렇게 활용할 수도 있넹
+# word = input()
+# small = input()
+# sp_word = word.split(small)
+
+# print(len(sp_word) - 1)
