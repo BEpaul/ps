6e71f236e3852be0e73461e5cd3e82615e5c3449
Minhyuk Kim <rhrlwld@gmail.com>
2023-09-08 17:55:46 +0900

    w2-p4-count-primes

---
+++ leetcode/pst/204.c
@@ -0,0 +1,18 @@
+// Sieve of Eratosthenes
+
+int isPrime[5000001];
+
+int countPrimes(int n) {
+    int i;
+    for (i = 2; i < n; i++) isPrime[i] = 1;
+
+    for (i = 2; i * i < n; i++) {
+        if (!isPrime[i]) continue;
+
+        for (int j = i * i; j < n; j += i) isPrime[j] = 0;
+    }
+
+    int count = 0;
+    for (i = 2; i < n; i++) count += isPrime[i];
+    return count; 
+}
