db9a501b53704a2d4f515311f858f4c439a5ae21
Minhyuk Kim <rhrlwld@gmail.com>
2024-02-22 23:11:20 +0900

    DP

---
+++ boj/11660.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[][] nums;
+    static int[][] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        nums = new int[n + 1][n + 1];
+        dp = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                nums[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = dp[i][j - 1] + nums[i][j];
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i][j] + dp[i - 1][j];
+            }
+        }
+
+        int x1, x2, y1, y2;
+        long result;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            x1 = Integer.parseInt(st.nextToken());
+            y1 = Integer.parseInt(st.nextToken());
+            x2 = Integer.parseInt(st.nextToken());
+            y2 = Integer.parseInt(st.nextToken());
+
+            result = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
+            bw.write(String.valueOf(result));
+            bw.newLine();
+        }
+
+        bw.flush();
+
+        br.close();
+        bw.close();
+
+    }
+}
