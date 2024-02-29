f60a33c7c882a9c2dd21de5d7599bb4850df5ad1
Minhyuk Kim <rhrlwld@gmail.com>
2024-03-01 01:04:45 +0900

    DP

---
+++ boj/9251.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    static String s1, s2;
+    static int[][] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        s1 = br.readLine();
+        s2 = br.readLine();
+
+        dp = new int[s2.length() + 1][s1.length() + 1];
+
+        for (int i = 1; i <= s2.length(); i++) {
+            for (int j = 1; j <= s1.length(); j++) {
+                if (s2.charAt(i - 1) == s1.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        bw.write(String.valueOf(dp[s2.length()][s1.length()]));
+
+        br.close();
+        bw.close();
+    }
+}
