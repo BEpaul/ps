7399bd97565ab567c4cea444cad17f3920eb7221
Minhyuk Kim <rhrlwld@gmail.com>
2024-02-29 11:29:49 +0900

    플로이드 워셜

---
+++ boj/11404.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final int INF = 987654321;
+    static int n, m;
+    static int[][] dist;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        dist = new int[n + 1][n + 1];
+        for (int[] m : dist) {
+            Arrays.fill(m, INF);
+        }
+
+        int s, e, c;
+        StringTokenizer st;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            s = Integer.parseInt(st.nextToken());
+            e = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+
+            dist[s][e] = Math.min(dist[s][e], c);
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (i == j) {
+                        continue;
+                    }
+
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][j] == INF) {
+                    bw.write("0 ");
+                } else {
+                    bw.write(String.valueOf(dist[i][j]) + " ");
+                }
+            }
+            bw.newLine();
+        }
+
+        br.close();
+        bw.close();
+    }
+}
