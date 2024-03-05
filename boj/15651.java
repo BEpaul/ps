9b0e07712f714997925449355ffd067b39e4714b
Minhyuk Kim <rhrlwld@gmail.com>
2024-03-06 01:37:54 +0900

    백트래킹

---
+++ boj/15651.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+
+        dfs(0);
+        System.out.println(sb);
+
+        br.close();
+    }
+
+    static void dfs(int depth) {
+        if (depth == m) {
+            for (int a : arr) {
+                sb.append(a).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[depth] = i + 1;
+            dfs(depth + 1);
+        }
+    }
+}
