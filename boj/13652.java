f9d4db2d810d26454cfd2b4c0cd9528a18903c3c
Minhyuk Kim <rhrlwld@gmail.com>
2024-03-08 23:49:24 +0900

    백트래킹

---
+++ boj/13652.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        visited = new boolean[n];
+
+        dfs(0, 0);
+        System.out.println(sb);
+
+        br.close();
+        bw.close();
+    }
+
+    static void dfs(int depth, int start) {
+        if (depth == m) {
+            for (int a : arr) {
+                sb.append(a).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            arr[depth] = i + 1;
+            dfs(depth + 1, i);
+        }
+    }
+}
